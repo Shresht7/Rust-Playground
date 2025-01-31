e50690788bf4fceb953e06d8cf18c7aa919d1511
Shresht Srivastav <59516096+Shresht7@users.noreply.github.com>
2025-02-01 00:10:16 +0530

    Day 2 Rustlings: Modules, HashMaps, Options, ErrorHandling

--- 01_rustlings/exercises/quizzes/quiz2.rs
+++ 01_rustlings/exercises/quizzes/quiz2.rs
@@ -27,7 +27,25 @@ mod my_module {
     use super::Command;
 
     // TODO: Complete the function as described above.
-    // pub fn transformer(input: ???) -> ??? { ??? }
+    pub fn transformer(input: Vec<(String, Command)>) -> Vec<String> {
+        let mut res: Vec<String> = Vec::new();
+        for (mut str, cmd) in input {
+            match cmd {
+                Command::Uppercase => {
+                    res.push(str.to_ascii_uppercase().to_owned());
+                }
+                Command::Trim => {
+                    res.push(str.trim().to_owned());
+                }
+                Command::Append(n) => {
+                    let s = "bar".repeat(n);
+                    str.push_str(&s);
+                    res.push(str.to_owned());
+                }
+            }
+        }
+        return res;
+    }
 }
 
 fn main() {
@@ -37,7 +55,7 @@ fn main() {
 #[cfg(test)]
 mod tests {
     // TODO: What do we need to import to have `transformer` in scope?
-    // use ???;
+    use super::my_module::transformer;
     use super::Command;
 
     #[test]

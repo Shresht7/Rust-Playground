560a0a1877b682b018242fea1124bd8aeb18cd2f
Shresht Srivastav <59516096+Shresht7@users.noreply.github.com>
2025-01-31 11:43:14 +0530

    Catch All Statements

--- 00_TheBook/16_match_control_flow/src/main.rs
+++ 00_TheBook/16_match_control_flow/src/main.rs
@@ -28,6 +28,73 @@ fn value_in_cents(coin: Coin) -> u8 {
 
 fn matchControlFlow() {}
 
+// Another useful feature of match arms is that they can bind values. This is how we can extract values out from an enum variant.
+
+#[derive(Debug)] // so we can inspect the state in a minute
+enum UsState {
+    Alabama,
+    Alaska,
+    // --snip--
+}
+
+enum Coin {
+    Penny,
+    Nickel,
+    Dime,
+    Quarter(UsState),
+}
+
+fn value_in_cents(coin: Coin) -> u8 {
+    match coin {
+        Coin::Penny => 1,
+        Coin::Nickel => 5,
+        Coin::Dime => 10,
+        Coin::Quarter(state) => {
+            println!("State quarter from {state:?}!");
+            25
+        }
+    }
+}
+
+// Matching with Option<T>
+
+// This is how we handle NULL values in Rust.
+
+fn plus_one(x: Option<i32>) -> Option<i32> {
+    match x {
+        None => None,
+        Some(i) => Some(i + 1),
+    }
+}
+
+fn match_option() {
+    let five = Some(5);
+    let six = plus_one(five);
+    let none = plus_one(None);
+}
+
+// Combined, match and enums are extremely useful in many situations. You'll see them come up a lot in Rust code.
+
+// One other thing about match; They are exhaustive. i.e. they must cover all possible cases.
+// We can use catch all patterns, when we want an expression to apply for any kind of match.
+
+fn catch_all() {
+    let dice_roll = 9;
+    match dice_roll {
+        3 => add_fancy_hat(),
+        7 => remove_fancy_hat(),
+        other => move_player(other),
+    }
+    // The first two arms, the patterns are the literal values of 3 and 7. The last arm covers all other possible
+    // values. We've chosen to name it `other`. If we don't want to capture the pattern in a variable we can use _ to
+    // indicate that we are not interested in storing the value as a variable.
+}
+
+fn add_fancy_hat() {}
+fn remove_fancy_hat() {}
+fn move_player(num_spaces: u8) {}
+
 fn main() {
-    matchControlFlow()
+    matchControlFlow();
+    value_in_cents(Coin::Quarter(UsState::Alaska));
 }

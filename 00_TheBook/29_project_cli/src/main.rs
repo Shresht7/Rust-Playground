aee4f2374d0b51c083ecc701720bfe0c93f55e3b
Shresht Srivastav <59516096+Shresht7@users.noreply.github.com>
2025-02-01 10:14:36 +0530

    Improve argument parsing with error handling in Config struct

--- 00_TheBook/29_project_cli/src/main.rs
+++ 00_TheBook/29_project_cli/src/main.rs
@@ -1,11 +1,14 @@
 // $ cargo run -- searchstring example-filename.txt
 
-use std::{env, fs};
+use std::{env, fs, process};
 
 fn main() {
     // Retrieve the command-line arguments that were passed into this program
     let args: Vec<String> = env::args().collect();
-    let config = Config::new(&args);
+    let config = Config::build(&args).unwrap_or_else(|err| {
+        println!("Problem parsing arguments: {err}");
+        process::exit(1);
+    });
 
     // cargo run -- needle haystack
     println!("Searching for {}", config.query);
@@ -25,9 +28,12 @@ struct Config {
 }
 
 impl Config {
-    fn new(args: &[String]) -> Config {
+    fn build(args: &[String]) -> Result<Config, &'static str> {
+        if args.len() < 3 {
+            return Err("not enough arguments");
+        }
         let query = args[1].clone();
         let filepath = args[2].clone();
-        Config { query, filepath }
+        Ok(Config { query, filepath })
     }
 }

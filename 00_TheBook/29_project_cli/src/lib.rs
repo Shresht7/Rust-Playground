caaaae598cf3e6f5e1c820085e987fad01a3dec9
Shresht Srivastav <59516096+Shresht7@users.noreply.github.com>
2025-02-01 10:36:46 +0530

    Implement search functionality to find query matches in file contents

--- 00_TheBook/29_project_cli/src/lib.rs
+++ 00_TheBook/29_project_cli/src/lib.rs
@@ -1,10 +1,10 @@
 use std::{error, fs};
 
 pub fn run(config: Config) -> Result<(), Box<dyn error::Error>> {
-    // Read file contents
     let contents = fs::read_to_string(config.filepath)?;
-    // cargo run -- needle poem.txt
-    println!("With text:\n{contents}");
+    for line in search(&config.query, &contents) {
+        println!("{line}");
+    }
     Ok(())
 }
 
@@ -21,5 +21,33 @@ impl Config {
         let query = args[1].clone();
         let filepath = args[2].clone();
         Ok(Config { query, filepath })
+    }
+}
+
+pub fn search<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
+    let mut results = Vec::new();
+
+    for line in contents.lines() {
+        if line.contains(query) {
+            results.push(line);
+        }
+    }
+
+    results
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn one_result() {
+        let query = "duct";
+        let contents = "\
+Rust:
+safe, fast, productive.
+Pick three.";
+
+        assert_eq!(vec!["safe, fast, productive."], search(query, contents));
     }
 }

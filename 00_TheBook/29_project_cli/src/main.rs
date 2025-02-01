47d3331660bfbe29d1ca7aa535334386de11d07d
Shresht Srivastav <59516096+Shresht7@users.noreply.github.com>
2025-02-01 10:01:31 +0530

    Extract query and filename from arguments

--- 00_TheBook/29_project_cli/src/main.rs
+++ 00_TheBook/29_project_cli/src/main.rs
@@ -3,6 +3,14 @@
 use std::env;
 
 fn main() {
+    // Retrieve the command-line arguments that were passed into this program
     let args: Vec<String> = env::args().collect();
-    dbg!(args);
+
+    // Extract the query and filepath from the arguments
+    let query = &args[1];
+    let filepath = &args[2];
+
+    // cargo run -- needle haystack
+    println!("Searching for {query}");
+    println!("In file {filepath}");
 }

eb8f181b8f2ff898ea3bf13f101bffc83d9eb289
Shresht Srivastav <59516096+Shresht7@users.noreply.github.com>
2025-02-01 10:11:27 +0530

    Refactor command-line argument handling into a Config struct

--- 00_TheBook/29_project_cli/src/main.rs
+++ 00_TheBook/29_project_cli/src/main.rs
@@ -5,18 +5,29 @@ use std::{env, fs};
 fn main() {
     // Retrieve the command-line arguments that were passed into this program
     let args: Vec<String> = env::args().collect();
-
-    // Extract the query and filepath from the arguments
-    let query = &args[1];
-    let filepath = &args[2];
+    let config = Config::new(&args);
 
     // cargo run -- needle haystack
-    println!("Searching for {query}");
-    println!("In file {filepath}");
+    println!("Searching for {}", config.query);
+    println!("In file {}", config.filepath);
 
     // Read file contents
-    let contents = fs::read_to_string(filepath).expect("Should have been able to read the file");
+    let contents =
+        fs::read_to_string(config.filepath).expect("Should have been able to read the file");
 
     // cargo run -- needle poem.txt
     println!("With text:\n{contents}");
+}
+
+struct Config {
+    query: String,
+    filepath: String,
+}
+
+impl Config {
+    fn new(args: &[String]) -> Config {
+        let query = args[1].clone();
+        let filepath = args[2].clone();
+        Config { query, filepath }
+    }
 }

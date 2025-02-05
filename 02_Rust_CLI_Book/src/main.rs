cc768afc0d1b965ca63d475b0d78b2de7ae3bc16
Shresht Srivastav <59516096+Shresht7@users.noreply.github.com>
2025-02-05 23:21:13 +0530

    Update main function to accept and display command-line arguments for pattern and path

--- 02_Rust_CLI_Book/src/main.rs
+++ 02_Rust_CLI_Book/src/main.rs
@@ -1,3 +1,5 @@
 fn main() {
-    println!("Hello, world!");
+    let pattern = std::env::args().nth(1).expect("No pattern given");
+    let path = std::env::args().nth(2).expect("No path given");
+    println!("{pattern} {path}");
 }

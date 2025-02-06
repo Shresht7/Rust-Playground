4156b12bbc2b32b4111bbdff45a0b42c9384faf0
Shresht Srivastav <59516096+Shresht7@users.noreply.github.com>
2025-02-06 10:53:07 +0530

    03_Rust_by_Example: 00 Hello World

---
+++ 03_Rust_by_Example/00_Hello_World/src/main.rs
@@ -0,0 +1,15 @@
+// This is a comment, and is ignored by the compiler.
+// This is the main function.
+fn main() {
+    // Statements here are executed when the compiled binary is called.
+
+    // Print text to the console.
+    println!("Hello World!");
+
+    // println! is a macro that prints to the console
+}
+
+// This can be run using `cargo run`
+// or, by `cargo build` and then manually running the executable
+
+// or by compiling the code with `rustc` and then running the binary

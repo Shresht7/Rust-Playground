0dc9de22c5902744b61286cf7a4d69884e2380af
Shresht Srivastav <59516096+Shresht7@users.noreply.github.com>
2025-02-06 10:15:03 +0530

    Implement find_matches function and add unit test for pattern matching

--- 02_Rust_CLI_Book/src/main.rs
+++ 02_Rust_CLI_Book/src/main.rs
@@ -27,6 +27,17 @@ fn main() -> Result<()> {
     let mut writer = BufWriter::new(io::stdout().lock());
 
     log::debug!("Starting scan loop...");
+    find_matches(&mut reader, &args.pattern, &mut writer)?;
+
+    log::info!("Finished processing file");
+    Ok(())
+}
+
+fn find_matches<R, W>(reader: &mut R, pattern: &str, writer: &mut W) -> Result<()>
+where
+    R: BufRead,
+    W: Write,
+{
     loop {
         let mut buffer = String::new();
         let n = reader
@@ -35,12 +46,23 @@ fn main() -> Result<()> {
         if n <= 0 {
             break;
         }
-        if buffer.contains(&args.pattern) {
+        if buffer.contains(&pattern) {
             log::debug!("Found pattern in line: {}", buffer.trim_end());
             write!(writer, "{}", buffer)?;
         }
     }
-
-    log::info!("Finished processing file");
     Ok(())
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn find_a_match() {
+        let mut result = Vec::new();
+        let mut reader = "test contents\n".as_bytes();
+        find_matches(&mut reader, "test", &mut result).unwrap();
+        assert_eq!(result, b"test contents\n")
+    }
 }

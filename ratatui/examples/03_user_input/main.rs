bd329742efb08c5fc3436c59c18ea621acce8b0d
Shresht Srivastav <59516096+Shresht7@users.noreply.github.com>
2026-01-28 18:40:06 +0530

    Use `default` constructor

--- ratatui/examples/03_user_input/main.rs
+++ ratatui/examples/03_user_input/main.rs
@@ -2,7 +2,7 @@ mod app;
 use app::App;
 
 fn main() -> std::io::Result<()> {
-    let mut app = App::new("test");
+    let mut app = App::default();
     ratatui::run(|terminal| app.run(terminal))?;
     Ok(())
 }

71b012ba16079ec8b8628c189ceabf5563f0b881
Shresht Srivastav <59516096+Shresht7@users.noreply.github.com>
2026-04-16 22:47:17 +0530

    Enhance README and example with additional context and usage notes for Rocket framework

--- libraries/Rocket/README.md
+++ libraries/Rocket/README.md
@@ -2,7 +2,12 @@
 
 https://rocket.rs/
 
-Rocket is a web framework for Rust. 
+Rocket is a web framework for Rust. It provides primitives to build web servers and applications in Rust.
+
+> [!NOTE]
+> Rocket v0.5 uses the `tokio` runtime.
+
+---
 
 ## Install
 
@@ -19,3 +24,10 @@ or you can use Cargo to add it directly:
 ```bash
 cargo add rocket
 ```
+
+## Usage
+
+See [`00_hello_world`](examples/00_hello_world/main.rs) for a simple example of using Rocket to create a web server that responds with "Hello, world!" when accessed at the root URL (`/`).
+
+---
+
--- libraries/Rocket/examples/00_hello_world/main.rs
+++ libraries/Rocket/examples/00_hello_world/main.rs
@@ -4,8 +4,13 @@
 //!
 //! Run this example using `cargo run --example 00_hello_world` from the root of the project.
 
+// Explicitly importing with `#[macro_use]` allows us to use Rocket's macros globally anywhere in the application
+// without needing to prefix them with `rocket::`. This is necessary for the `#[get]` and `#[launch]` macros used in this example.
 #[macro_use]
 extern crate rocket;
+
+// Alternatively, we could have used regular imports for `rocket::get` and `rocket::launch`
+// or directly use `#[rocket::get]` and `#[rocket::launch]` in the code without the `#[macro_use]` declaration.
 
 /// Responds to GET requests at the root URL ("/") with a static string.
 #[get("/")]
@@ -14,7 +19,24 @@ fn index() -> &'static str {
 }
 
 /// Launches the Rocket web server and mounts the `index` route at the root URL ("/").
+/// Rocket begins serving requests after being _launched_, which starts a multi-threaded asynchronous server
+/// and dispatches requests to the appropriate routes as needed.
+/// Tip: #[launch] infers the return type of the function, so we can use `_` instead of explicitly writing `Rocket<Build>`.
+/// See https://rocket.rs/guide/v0.5/overview/#launching for more details.
 #[launch]
 fn rocket() -> _ {
+    // Before Rocket can dispatch requests to a route, the route needs to be _mounted_.
+    // Mounting a route means associating it with a base path. In this case, we mount the `index` route at the root path ("/").
+    // See https://rocket.rs/guide/v0.5/overview/#mounting for more details.
     rocket::build().mount("/", routes![index])
 }
+
+// Alternatively, we could use the `#[rocket::main]` route attribute instead of `#[launch]
+// #[rocket::main]
+// async fn main() -> Result<(), rocket::Error> {
+//     let _rocket = rocket::build()
+//         .mount("/hello", routes![world])
+//         .launch()
+//         .await?;
+//     Ok(())
+// }

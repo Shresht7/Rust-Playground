ba39182836f2595a7f56763b45cb1aec5127e474
Shresht Srivastav <59516096+Shresht7@users.noreply.github.com>
2026-04-21 19:56:45 +0530

    Rocket: `18_databases`

--- libraries/Rocket/examples/18_databases/main.rs
+++ libraries/Rocket/examples/18_databases/main.rs
@@ -1,1 +1,31 @@
-// TODO: Create Database Example
+// # Database
+
+// Rocket includes builtin, ORM-agnostic support for databases via `rocket_db_pools`. The library simplifies accessing one or more databases via
+// connection pools: data structures that maintain active database connections for use in the application.
+
+#[macro_use]
+extern crate rocket;
+
+use rocket_db_pools::sqlx::{self, Row};
+use rocket_db_pools::{Connection, Database};
+
+#[derive(Database)]
+#[database("sqlite_logs")]
+struct Logs(sqlx::SqlitePool);
+
+#[get("/<id>")]
+async fn read(mut db: Connection<Logs>, id: i64) -> Option<String> {
+    sqlx::query("SELECT content FROM logs WHERE id = ?")
+        .bind(id)
+        .fetch_one(&mut **db)
+        .await
+        .and_then(|row| Ok(row.try_get(0)?))
+        .ok()
+}
+
+#[launch]
+fn rocket() -> _ {
+    rocket::build()
+        .attach(Logs::init())
+        .mount("/", routes![read])
+}
